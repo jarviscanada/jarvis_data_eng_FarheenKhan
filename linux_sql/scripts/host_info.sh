2e98ec6b5c2dc3053d79c534ae7b5d586e62badd
farheen Khan <famikh1988@gmail.com>
2024-01-25 23:32:00 +0000

    Completed Task

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+lscpu_out=`lscpu`
+hostname=$(hostname -f)
+
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print $3,$4,$5,$6,$7}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' |  cut -c 1-3 | xargs)
+total_mem=$(vmstat --unit M | tail -1 | awk '{print $4}')
+timestamp=$(date +'%Y-%m-%d %k:%M:%S')
+
+insert_stmt="INSERT INTO host_info( hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem)
+      VALUES('$hostname','$cpu_number','$cpu_architecture','$cpu_model','$cpu_mhz','$l2_cache','$timestamp','$total_mem')"
+
+echo  "$insert_stmt"
+export PGPASSWORD=$psql_password
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
+exit $?
---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+ipsql_password=$5
+
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+echo "Hostname : $hostname"
+
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb"  | awk '{print $15}' | tail -n1 | xargs)
+cpu_kernel=$( echo "$vmstat_mb"  | awk '{print $14}' | tail -n1 | xargs)
+disk_io=$( echo "$vmstat_mb"  | awk '{print $10}' | tail -n1 | xargs)
+disk_available=$(echo "$(df -BM)" | awk 'NR==6 {print $4}' | rev | cut -c2- | rev | xargs)
+timestamp=$(vmstat -t | awk '{print $18" "$19}' | tail -n1 | xargs)
+
+
+host_id=$(psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -t -c "SELECT id FROM host_info WHERE hostname='$hostname'")
+echo "Host Id: $host_id"
+
+# Check if the variable is empty
+if [ -z "$host_id" ]; then
+    echo "host_id  Variable is empty"
+else
+    # Check if the variable contains only digits (integer)
+    if [[ "$host_id" =~ ^[0-9]+$ ]]; then
+        echo "Host-Id is: $host_id"
+
+        export PGPASSWORD=$psql_password
+        insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', 1, $memory_free, $cpu_idle,$cpu_kernel, $disk_io, $disk_available);"
+
+        echo $insert_stmt
+
+        psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
+    else
+        echo "Host-Id  does not contain an integer"
+    fi
+fi
+
+exit $?
+
